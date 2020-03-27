52b6e49e54118ec18a2280329c310e1cb1fa6dfe
Filippo Volpe <filippovolpe98@gmail.com>
2020-03-27 10:34:23 +0100

    added map vector

--- include/functions.h
+++ include/functions.h
@@ -77,7 +77,10 @@ void tocsv(const std::vector<std::pair<std::string, std::vector<double>>>& data,
 void tocsv(const std::vector<std::pair<std::string, std::vector<double>>>& data, const std::string& fname,
     const int digits=5);
 
+// vector manipulation
+void map(std::vector<double>& vec,std::function<double(uint32_t)> f);
 void arange(std::vector<double>& vec, const double start, const double step);
 void linespace(std::vector<double>& vec, const double xmin, const double xmax);
+void fill(std::vector<double>& vec,double val);
 
 #endif //__FUNCTIONS_H__
