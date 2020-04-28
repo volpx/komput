5cd8d74b4569214432a13041fdfb5b5d07d9f396
Filippo Volpe <filippovolpe98@gmail.com>
2020-04-28 17:26:01 +0200

    first revision of LU sover

---
+++ include/lu_dec.h
@@ -0,0 +1,56 @@
+#ifndef __LU_DEC_H__
+#define __LU_DEC_H__
+
+#include <vector>
+#include <cstdint>
+#include <functional>
+
+/* LU decomposition solver:
+ * The solution u' has Nx points (i from 0 to Nx-1)
+ * of which u[0] and u[Nx-1] are the border points
+ * given by qmin(m), qmax(m) functions.
+ * The matrix of the single evolution is of the form:
+ *  | d0(1) dp(1)                               |  u'[1]        u[1]       dn(0)qmin(m+1)
+ *  | dn(2) d0(2) dp(2)                         |  u'[2]        u[2]            0
+ *  |        ..............                     | (  ...   ) = ( ...   )-(      0          )
+ *  |       dn(Nx-3) d0(Nx-3) dp(Nx-3)          |  u'[Nx-3]     u[Nx-3]         0
+ *  |                dn(Nx-2) d0(Nx-2) dp(Nx-2) |  u'[Nx-2]     u[Nx-2]   dp(Nx-1)qmax(m+1)
+ * Where u' is the evolved vector (at time m+1)
+ * and u is the vector to evolve from (at time m)
+ * Info callabilities:
+ *  * qmin,qmax are callable for every index m of time you need
+ *  * dn(n=[0,...,Nx-2])
+ *  * d0(n=[1,...,Nx-2])
+ *  * dp(n=[1,...,Nx-1])
+ */
+class LU_Solver{
+	public:
+	LU_Solver(
+		const uint32_t Nx,
+		const std::function<double(uint32_t)> qmin,
+		const std::function<double(uint32_t)> qmax);
+	~LU_Solver();
+	void step_setup(
+		const std::function<double(uint32_t)> dn,
+		const std::function<double(uint32_t)> d0,
+		const std::function<double(uint32_t)> dp
+	);
+	void step(
+		const uint32_t m0,
+		const std::vector<double> *u_p0,
+		std::vector<double> *u_p1
+	);
+
+	private:
+	const uint32_t Nx;
+	std::function<double(uint32_t)> qmin;
+	std::function<double(uint32_t)> qmax;
+	std::vector<double> en;
+	std::vector<double> e0;
+	std::vector<double> ep;
+	std::vector<double> xx;
+	double dn_0;
+	double dp_Nx_1;
+};
+
+#endif // __LU_DEC_H__
---
+++ src/lu_dec.cpp
@@ -0,0 +1,57 @@
+#include "lu_dec.h"
+
+LU_Solver::LU_Solver(
+	const uint32_t Nx,
+	const std::function<double(uint32_t)> qmin,
+	const std::function<double(uint32_t)> qmax ):
+	Nx{Nx}, qmin{qmin}, qmax{qmax},
+	en(Nx-3), e0(Nx-2), ep(Nx-3), xx(Nx-2)
+	{
+}
+LU_Solver::~LU_Solver(){
+
+}
+
+void LU_Solver::step_setup(
+	const std::function<double(uint32_t)> dn,
+	const std::function<double(uint32_t)> d0,
+	const std::function<double(uint32_t)> dp)
+	{
+	// Calculate the coefficients of the decomposition
+	ep[0]=dp(1);
+	e0[0]=d0(1);
+	en[0]=dn(2)/e0[0];
+	for(uint32_t n=1;n<Nx-3;++n){
+		ep[n]=dp(n+1);
+		e0[n]=d0(n+1)-dn(n+1)*ep[n-1]/e0[n-1];
+		en[n]=dn(n+2)/e0[n];
+	}
+	e0[Nx-3]=d0(Nx-3+1)-dn(Nx-3+1)*ep[Nx-3-1]/e0[Nx-3-1];
+
+	dn_0=dn(0);
+	dp_Nx_1=dp(Nx-1);
+}
+
+void LU_Solver::step(
+	const uint32_t m0,
+	const std::vector<double> *u_p0,
+	std::vector<double> *u_p1)
+	{
+
+	// Eq. 61
+	xx[0]=(*u_p0)[1]-dn_0*qmin(m0+1);
+	for(uint32_t n=1;n<Nx-3;++n){
+		xx[n]=(*u_p0)[n+1]-en[n-1]*xx[n-1];
+	}
+	xx[Nx-3]=(*u_p0)[Nx-3+1]-dp_Nx_1*qmax(m0+1)-en[Nx-3-1]*xx[Nx-3-1];
+
+	// eq. 64
+	(*u_p1)[Nx-1]=qmax(m0+1);
+	(*u_p1)[Nx-2]=xx[Nx-3]/e0[Nx-3];
+	for(uint32_t n=1;n<Nx-2;++n){
+		(*u_p1)[Nx-n-2]=xx[Nx-n-3]/e0[Nx-n-3]
+			-ep[Nx-n-3]/e0[Nx-n-3]*(*u_p1)[Nx-n-1];
+	}
+	(*u_p1)[0]=qmin(m0+1);
+
+}
