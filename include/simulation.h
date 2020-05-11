bd7710218dd6e4abe768474cb7897208ece27492
Filippo Volpe <filippovolpe98@gmail.com>
2020-05-11 16:33:32 +0200

    switching branch

--- include/simulation.h
+++ include/simulation.h
@@ -1,11 +1,14 @@
 #ifndef __SIMULATION_H__
 #define __SIMULATION_H__
 
+#include <cstdint>
 #include <cmath>
+#include <vector>
 
-class Vec3D{
+class Vec3D
+{
 public:
-	Vec3D(double x=0,double y=0,double z=0);
+	Vec3D(double x = 0, double y = 0, double z = 0);
 
 	double x;
 	double y;
@@ -18,6 +21,9 @@ public:
 	Vec3D Vec3D::getNormalized() const;
 
 	friend Vec3D operator-(const Vec3D &a, const Vec3D &b);
+	friend Vec3D operator+(const Vec3D &a, const Vec3D &b);
 };
+
+int init_born_von_karman_3d(double L, std::vector<Vec3D> q);
 
 #endif // __SIMULATION_H__
--- src/simulation.cpp
+++ src/simulation.cpp
@@ -1,27 +1,25 @@
 #include "simulation.h"
 
-Vec3D::Vec3D(double x,double y,double z):
-	x{x}, y{y}, z{z}
+Vec3D::Vec3D(double x, double y, double z) : x{x}, y{y}, z{z}
 {
-
 }
 
 double Vec3D::norm() const
 {
-	return std::sqrt(this->x*this->x+this->y*this->y+this->y*this->y);
+	return std::sqrt(this->x * this->x + this->y * this->y + this->y * this->y);
 }
 
 double Vec3D::norm2() const
 {
-	return this->x*this->x+this->y*this->y+this->y*this->y;
+	return this->x * this->x + this->y * this->y + this->y * this->y;
 }
 
 void Vec3D::normalize()
 {
 	double n{this->norm()};
-	this->x/=n;
-	this->y/=n;
-	this->z/=n;
+	this->x /= n;
+	this->y /= n;
+	this->z /= n;
 }
 
 Vec3D Vec3D::getNormalized() const
@@ -34,5 +32,26 @@ Vec3D Vec3D::getNormalized() const
 
 Vec3D operator-(const Vec3D &a, const Vec3D &b)
 {
-	return Vec3D{a.x-b.x, a.y-b.y, a.y-b.y};
+	return Vec3D{a.x - b.x, a.y - b.y, a.y - b.y};
+}
+
+Vec3D operator+(const Vec3D &a, const Vec3D &b)
+{
+	return Vec3D{a.x + b.x, a.y + b.y, a.y + b.y};
+}
+
+int init_born_von_karman_3d(double L, std::vector<Vec3D> q)
+{
+	size_t N = q.size();
+	double dl = L / std::pow(N, 1.0 / 3);
+
+	for (size_t i = 0;; i++)
+	{
+		for (size_t j = 0;; j++)
+		{
+			for (size_t k = 0;; k++)
+			{
+			}
+		}
+	}
 }
